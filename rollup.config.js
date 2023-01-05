d606b18a30045fcfd4eb148e7f86d127618b3c0b
Bruno Sajermann <sajermannbruno@gmail.com>
2023-01-04 21:39:54 -0300

    adjusts

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,20 @@
-import typescript from '@rollup/plugin-typescript';
-import terser from '@rollup/plugin-terser';
+import ts from 'rollup-plugin-ts';
 
 export default [
 	{
-		preserveModules: true,
 		input: 'src/index.ts',
 		output: {
 			dir: 'build',
 			format: 'esm',
 		},
 		plugins: [
-			typescript({
+			ts({
 				tsconfig: './tsconfig.json',
 				compilerOptions: {
 					jsx: 'react-jsx',
 					declaration: true,
 					declarationDir: 'build',
 				},
-				exclude: [
-					'src/Pages',
-					'src/Config',
-					'src/App.tsx',
-					'src/main.tsx',
-					'src/**/*.spec.ts',
-					'src/**/*.spec.tsx',
-					'src/**/*.test.ts',
-					'src/**/*.test.tsx',
-				],
-			}),
-			terser({
-				maxWorkers: 4,
 			}),
 		],
 	},
